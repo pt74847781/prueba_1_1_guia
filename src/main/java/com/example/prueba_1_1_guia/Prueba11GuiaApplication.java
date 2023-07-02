8e4dd928cf81efc9853831d86cbaa978770f0554
pt74847781 <134320456+pt74847781@users.noreply.github.com>
2023-07-02 21:28:57 +0000

    primercommit

---
+++ src/main/java/com/example/prueba_1_1_guia/Prueba11GuiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.prueba_1_1_guia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prueba11GuiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prueba11GuiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/prueba_1_1_guia/Prueba11GuiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prueba_1_1_guia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prueba11GuiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
